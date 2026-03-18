df10aa26fbb32e541ee4e1321c62da7ce57c588f
Yar00myr <ivanucaromir@gmail.com>
2026-03-18 22:23:51 +0000

    fix: update helper

--- backend/app/routes/submissions.py
+++ backend/app/routes/submissions.py
@@ -2,6 +2,8 @@ from fastapi import status, HTTPException
 from fastapi.routing import APIRouter
 from sqlalchemy import select, update
 from sqlalchemy.orm import selectinload
+from sqlalchemy.exc import IntegrityError
+
 from app.dependencies import SessionDep
 from app.models import Submission, SubmissionUrl, SubmissionUrlOption, Team
 from app.schemas import SubmissionModel, SubmissionUrlOptionModel, SubmissionUrlModel
@@ -10,7 +12,13 @@ router = APIRouter(prefix="/submissions", tags=["submissions"])
 
 
 async def get_submission(team_id: int, session: SessionDep) -> Submission:
-    submission = await session.get(Submission, team_id)
+    statement = (
+        select(Submission)
+        .where(Submission.team_id == team_id)
+        .options(selectinload(Submission.urls).selectinload(SubmissionUrl.url))
+    )
+    result = await session.execute(statement)
+    submission = result.scalar_one_or_none()
 
     if submission is None:
         raise HTTPException(
@@ -34,16 +42,39 @@ async def submission(team_id: int, session: SessionDep):
     return await get_submission(team_id, session)
 
 
-@router.post("/", response_model=SubmissionModel, status_code=status.HTTP_201_CREATED)
-async def create_submission(submission_data: SubmissionModel, session: SessionDep):
-    pass
+# Not ready yet
+
+# @router.post("/", response_model=SubmissionModel, status_code=status.HTTP_201_CREATED)
+# async def create_submission(submission_data: SubmissionModel, session: SessionDep):
+#     team = await session.get(Team, submission_data.team_id)
+#     if not team:
+#         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Team not found")
+
+#     new_submission = Submission(team_id=submission_data.team_id)
+
+#     for url_item in submission_data.urls:
+#         new_submission.urls.append(SubmissionUrl(url_id=url_item.url_id))
+
+#     session.add(new_submission)
+
+#     try:
+#         await session.commit()
+#         await session.refresh(new_submission)
+#     except IntegrityError:
+#         await session.rollback()
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST,
+#             detail="Submission for this team already exists",
+#         )
+
+#     return new_submission
 
 
-@router.patch(
-    "/{team_id}/", response_model=SubmissionModel, status_code=status.HTTP_200_OK
-)
-async def update_submission():
-    pass
+# @router.patch(
+#     "/{team_id}/", response_model=SubmissionModel, status_code=status.HTTP_200_OK
+# )
+# async def update_submission():
+#     pass
 
 
 @router.delete("/{team_id}/", status_code=status.HTTP_204_NO_CONTENT)
