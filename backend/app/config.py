5a3f062ea701b37ca38b91084e67eca2666e7c0e
Yar00myr <ivanucaromir@gmail.com>
2026-04-23 22:24:45 +0100

    refactor: move firebase initialization to config class

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,16 +1,22 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../.env", extra='ignore')
+    model_config = SettingsConfigDict(env_file=".env", extra='ignore')
+
     SECRET_KEY: str = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI")
+    FIREBASE_CERT_PATH: str = str(BASE_DIR / "serviceAccountKey.json")
+    
     # TODO: Move this to a config file
     CORS_ORIGINS: list[str] = [
         "http://localhost",
         "http://localhost:5173",
     ]
-    SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI")
     ROLE_REQUEST_NOTIFICATION_MESSAGE: str = '''
     A user $user requested a role $role. Do you approve this request?
     '''
--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -1,5 +1,5 @@
-import firebase_admin
-from pathlib import Path
-cert = Path('app/serviceAccountKey.json').resolve()
-cred = firebase_admin.credentials.Certificate(str(cert))
-firebase = firebase_admin.initialize_app(cred)
+from firebase_admin import credentials, initialize_app
+from app.config import settings
+
+cred = credentials.Certificate(settings.FIREBASE_CERT_PATH)
+firebase = initialize_app(cred)
