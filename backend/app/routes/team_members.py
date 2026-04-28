10d5c0fc5a93995369e38e303162e4fea985e100
Yar00myr <ivanucaromir@gmail.com>
2026-04-28 18:34:44 +0100

    fix: minor changes

--- backend/app/routes/team_members.py
+++ backend/app/routes/team_members.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import IntegrityError
 
 from app.dependencies import SessionDep
 from app.models import TeamMember
-from app.schemas import TeamMemberPublic, TeamMemberUpdate
+from app.schemas import TeamMemberCreate, TeamMemberPublic, TeamMemberUpdate
 from app.utils.routes import get_team, check_registration_open, get_tournament
 
 router = APIRouter(
