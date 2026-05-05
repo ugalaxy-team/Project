5eb3155d95282992b5412efef182b871e2ff9449
Fundi1330 <lerchmap@gmail.com>
2026-05-05 23:19:26 +0300

    refactor(utils): move get_role to app.utils

--- backend/app/routes/roles.py
+++ backend/app/routes/roles.py
@@ -1,17 +1,11 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter
 from app.schemas import RolePublic
 from app.models import Role
 from app.dependencies import SessionDep
 from sqlalchemy import select
+from app.utils import get_role
 
 router = APIRouter(prefix='/roles', tags=['roles'])
-
-async def get_role(name: str, session: SessionDep):
-    statement = select(Role).where(Role.name==name)
-    role = (await session.execute(statement)).scalar()
-    if not role:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='Role not found!')
-    return role
 
 @router.get('/', response_model=list[RolePublic])
 async def roles(session: SessionDep):
--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -4,3 +4,4 @@ from .notifications import send_notification
 from .users import get_user_by_email, get_user_by_firebase_uid, get_user_by_id
 from .tasks import get_requirements, get_task
 from .tournaments import get_tournament, tournament_load_options
+from .roles import get_role
