1364375e2f8ee8b6880e9d0266db313ea87c3d49
Fundi1330 <lerchmap@gmail.com>
2026-05-06 19:51:11 +0300

    feat: run tests and ruff before commit

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -16,7 +16,8 @@ engine = create_async_engine(
 AsyncTestingSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(
+        autouse=True)
 async def setup_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -28,6 +29,8 @@ async def setup_database():
 @pytest.fixture()
 async def db_session():
     async with AsyncTestingSessionLocal() as session:
+
+        
         yield session
 
 
