c9ef5f3655d3f6a8392dafa886e01434ab36e5e2
Fundi1330 <lerchmap@gmail.com>
2026-05-15 20:47:47 +0300

    test: add tests for leaderboard

---
+++ backend/tests/routes/test_leaderboard.py
@@ -0,0 +1,51 @@
+import pytest
+from app import app
+from app.dependencies import get_current_user
+from app.models import TaskEvaluationCriterion
+from tests.factories import (
+    JuryAssignmentStatusOptionFactory,
+    RoleFactory,
+    SubmissionUrlOptionFactory,
+    TaskFactory,
+    TaskStatusOptionFactory,
+    TeamFactory,
+    TeamMemberFactory,
+    TournamentFactory,
+    TournamentStatusOptionFactory,
+    UserFactory,
+    JuryAssignmentFactory,
+    JuryAssignmentStatusOptionFactory,
+    SubmissionFactory,
+    SubmissionEvaluationFactory,
+    TaskEvaluationCriterionFactory,
+    CriterionScoreFactory
+)
+from app.config import settings
+from app.utils import calculate_evaluation_total
+
+@pytest.mark.slow
+async def test_task_leaderboard(create, client, db_session):
+    j1 = await create(UserFactory)
+    j2 = await create(UserFactory)
+    finished = await create(TournamentStatusOptionFactory, name=settings.TOURNAMENT_STATUS_NAMES.FINISHED)
+    tournament = await create(TournamentFactory, status=finished)
+    closed = await create(TaskStatusOptionFactory, name=settings.TASK_STATUS_NAMES.SUBMISSION_CLOSED)
+    task = await create(TaskFactory, tournament=tournament, status=closed, min_reviews_per_submission=0)
+    captain = await create(TeamMemberFactory, tournament=tournament)
+    team = await create(TeamFactory, captain=captain, tournament=tournament)
+    s1 = await create(SubmissionFactory, task=task, team=team)
+    a1 = await create(JuryAssignmentFactory, task=task, submission=s1, jury=j1)
+    a2 = await create(JuryAssignmentFactory, task=task, submission=s1, jury=j2)
+    e1 = await create(SubmissionEvaluationFactory, jury=j1, assignment=a1, submission=s1)
+    e2 = await create(SubmissionEvaluationFactory, jury=j2, assignment=a2, submission=s1)
+    await db_session.refresh(e1, ['criterion_scores'])
+    await create(CriterionScoreFactory, score=7, evaluation=e1)
+    await create(CriterionScoreFactory, score=52, evaluation=e1)
+    e1_calculated = calculate_evaluation_total(e1)
+
+    resp = await client.get(f'/tournaments/{tournament.id}/tasks/{task.id}/leaderboard/')
+    assert resp.status_code == 200
+    total_scores = [e['total_score'] for e in resp.json()]
+    average_scores = [e['average_score'] for e in resp.json()]
+    assert e1_calculated in total_scores
+    assert e1_calculated in average_scores
